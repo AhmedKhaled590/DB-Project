345fb991f8ac5666b8f472f93214b8f8d8446486
AhmedKhaled <kahmd1444@gmail.com>
2021-01-05 15:56:05 +0200

    Add Page

--- routes/Admin.js
+++ routes/Admin.js
@@ -18,11 +18,11 @@ router.get('/Don', function(req, res, next) {
     if(err)throw err;
     User  = user;
   });
-    db.all('SELECT*from don_record R,DONOR D WHERE D.SSN = R.SSN',function(err, rows){
-      console.log(rows);
+    db.all('SELECT*from don_record R,DONOR D WHERE D.SSN = R.SSN ',function(err, rows){
     res.render('pages/Admin_DON',{title:"Blood Bank",css1:"home",css2:"style",css3:"animate",scrp:"home",Donations:rows,UserName:User.Fname})
 });
 });
+
 
 
 router.get('/Tests', function(req, res, next) {
@@ -35,12 +35,35 @@ router.get('/Orders', function(req, res, next) {
 
 
 router.get('/Inventory', function(req, res, next) {
-  res.render('pages/Inventory',{title:"Blood Bank",css1:"home",css2:"style",css3:"",scrp:"home"})
+    var sql ='SELECT*FROM INVENTORY I,DON_RECORD R ,DONOR D WHERE I.Sample_ID=R.Sample_ID AND R.SSN=D.SSN ';
+
+    var NumberOfDonations;
+    var NumberOfAAtype;
+    db.all('SELECT COUNT(*) as n FROM INVENTORY',function(err,num){
+      num.forEach(nd=>{
+        NumberOfDonations=nd.n;
+      })
+    })
+
+    db.all('SELECT COUNT(*) as n FROM INVENTORY where BLOOD_TYPE="A+"',function(err,num){
+      num.forEach(nd=>{
+        console.log(nd);
+        NumberOfAAtype=nd.n;
+      })
+    })
+
+
+    db.all(sql,[],function(err,inv){
+      res.render('pages/Inventory',{title:"Blood Bank",css1:"home",css2:"style",css3:"",scrp:"Inventory",Inventory:inv,numdon:NumberOfDonations,numaa:NumberOfAAtype})
+  
+    })
 });
 
 router.get('/Branches', function(req, res, next) {
   res.render('pages/Branches',{title:"Blood Bank",css1:"home",css2:"Preq",css3:"reg",scrp:"home"})
 });
+
+
 
 
 module.exports = router;
