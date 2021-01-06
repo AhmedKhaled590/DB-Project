f69432ab18296e598e04f188aeaf9a475f2a55ed
AhmedKhaled <kahmd1444@gmail.com>
2021-01-06 17:44:46 +0200

    Updates in DB

--- routes/Admin.js
+++ routes/Admin.js
@@ -21,13 +21,13 @@ router.get('/', function (req, res, next) {
 
 var tot;
 router.get('/Don', function (req, res, next) {
-  db.all('SELECT *from Donation_requests R,DONOR D WHERE D.SSN = R.SSN AND ( r.test_result!="CONFIRMED" OR r.test_result!="REJECTED") ', function (err, rows) {
+  db.all('SELECT *from Donation_requests R,DONOR D WHERE D.SSN = R.SSN AND r.test_result="QUEUED" ', function (err, rows) {
 
-    db.all('SELECT COUNT(*) AS n FROM DONATION_REQUESTS',[],(err,t)=>{
-      t.forEach(r=>{
+    db.all('SELECT COUNT(*) AS n FROM DONATION_REQUESTS r where r.test_result="QUEUED" ', [], (err, t) => {
+      t.forEach(r => {
         console.log(r.n);
-        tot=r;
-        res.render('pages/Admin_DON', { title: "Blood Bank", css1: "home", css2: "style", css3: "animate", scrp: "home", Donations: rows, UserName: User.Fname ,total:tot})
+        tot = r;
+        res.render('pages/Admin_DON', { title: "Blood Bank", css1: "home", css2: "style", css3: "animate", scrp: "home", Donations: rows, UserName: User.Fname, total: tot })
       })
     });
   });
@@ -35,8 +35,17 @@ router.get('/Don', function (req, res, next) {
 
 
 router.get('/DonRecords', function (req, res, next) {
-  db.all('SELECT * FROM DON_RECORD R ,DONOR D,num v WHERE D.SSN=R.SSN and r.ssn = v.ssn ', function (err, rows) {
-    res.render('pages/Admin_DONRecords', { title: "Blood Bank", css1: "home", css2: "style", css3: "animate", scrp: "home", Donations: rows, UserName: User.Fname })
+
+  db.all('SELECT*from Donation_To_Add', [], (err, Records) => {
+    Records.forEach(Record => {
+      db.all('Insert Into DON_RECORD (SSN,DONATION_DATE,REQUEST_ID) VALUES (?,?,?);', [Record.SSN, Record.DETERMINED_DATE, Record.ID], (err => {
+        db.all('UPDATE DONATION_REQUESTs SET TEST_RESULT ="FINISHED" WHERE ID = ?', [Record.ID]);
+      }));
+    });
+    
+    db.all('SELECT * FROM DON_RECORD R ,DONOR D,num v WHERE D.SSN=R.SSN and r.ssn = v.ssn ', function (err, rows) {
+      res.render('pages/Admin_DONRecords', { title: "Blood Bank", css1: "home", css2: "style", css3: "animate", scrp: "home", Donations: rows, UserName: User.Fname })
+    });
   });
 });
 
@@ -85,18 +94,18 @@ router.get('/Inventory', function (req, res, next) {
   })
 
   db.all('SELECT blood_type, COUNT(*) as n FROM INVENTORY group by blood_type', function (err, rows) {
-      sample = rows;
+    sample = rows;
   });
 
   var bestDonor;
-  db.all('select fname,d.ssn,count(*) as n from inventory i ,DON_RECORD d,donor dn WHERE i.Sample_ID=d.Sample_ID and d.ssn=dn.ssn GROUP by d.ssn,dn.fname ORDER by n DESC limit 1',(err,dn)=>{
-    dn.forEach(don=>{
-      bestDonor=don;
+  db.all('select fname,d.ssn,count(*) as n from inventory i ,DON_RECORD d,donor dn WHERE i.Sample_ID=d.Sample_ID and d.ssn=dn.ssn GROUP by d.ssn,dn.fname ORDER by n DESC limit 1', (err, dn) => {
+    dn.forEach(don => {
+      bestDonor = don;
     })
   })
 
   db.all(sql, [], function (err, inv) {
-    res.render('pages/Inventory', { title: "Blood Bank", css1: "home", css2: "style", css3: "", scrp: "Inventory", Inventory: inv, numdon: NumberOfDonations, samples: sample,donor:bestDonor })
+    res.render('pages/Inventory', { title: "Blood Bank", css1: "home", css2: "style", css3: "", scrp: "Inventory", Inventory: inv, numdon: NumberOfDonations, samples: sample, donor: bestDonor })
 
   })
 });
@@ -112,7 +121,7 @@ router.post('/Branches', function (req, res, next) {
   var Location = req.body.Location;
   var PhoneNum = req.body.PhoneNum;
   var BranchID = req.body.BranchID;
-  if (Location!=undefined && PhoneNum!=undefined) {
+  if (Location != undefined && PhoneNum != undefined) {
     db.all('Insert Into Branch (Location,Phone_Num) values (?,?)', [Location, PhoneNum], function (err) {
       if (err) console.log(err);
       else {
@@ -123,12 +132,12 @@ router.post('/Branches', function (req, res, next) {
     });
   }
 
-  else if (BranchID!=undefined) {
+  else if (BranchID != undefined) {
     db.all('delete FROM BRANCH where Branch_ID = ?', [BranchID], function (err, rows) {
       db.all('SELECT*FROM BRANCH', [], function (err, branches) {
         res.render('pages/Branches', { title: "Blood Bank", css1: "home", css2: "Preq", css3: "reg", scrp: "home", Branch: branches })
+      });
     });
-  });
   }
 
 });
