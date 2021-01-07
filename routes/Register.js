36c3dead6da8dcac9bbeb5e7a502b02cedb181b8
AhmedKhaled <kahmd1444@gmail.com>
2021-01-07 07:29:57 +0200

    Reg page add

--- routes/Register.js
+++ routes/Register.js
@@ -1,9 +1,40 @@
 var express = require('express');
+const { rows } = require('mssql');
 var router = express.Router();
+var db = require('../DB/DatabaseConfig');
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.render('pages/Register',{title:"Blood Bank",css1:"reg",css2:"",css3:"",scrp:"reg"})
+router.get('/', function (req, res, next) {
+  res.render('pages/Register', { title: "Blood Bank", css1: "reg", css2: "", css3: "", scrp: "reg" })
+});
+
+router.post('/', function (req, res, next) {
+  if (req.body.UserType = "Donor") {
+    var ssn = req.body.SSN;
+    var firstName = req.body.FirstName;
+    var Minit = req.body.Minit;
+    var lastName = req.body.LastName;
+    var email = req.body.email;
+    var password = req.body.password;
+    var confirmPassword = req.body.confirmPassword;
+    var bloodGroup = req.body.BloodGroup;
+    var gender = req.body.Gender;
+    var weight = req.body.Weight;
+    var num = req.body.PhoneNo;
+    var age = req.body.age;
+    var gov = req.body.Gov;
+    var address = req.body.Address;
+    db.all(`INSERT INTO DONOR(SSN,Fname,Minit,Lname,Age,Address,Phone_Num,Email,pass_word,Gender,Blood_Type)
+          VALUES(?,?,?,?,?,?,?,?,?,?,?)`,
+      [ssn, firstName, Minit, lastName, age, address, num, email, password, gender, bloodGroup],
+      (err) => {
+        if (err) console.log(err);
+        else {
+          res.render('pages/Home', { title: "Blood Bank", css1: "home", css2: "Preq", css3: "animate", scrp: "home" })
+        }
+      })
+  }
 });
 
 module.exports = router;
