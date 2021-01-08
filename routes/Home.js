2580c26d50a8c16fd863989814e232459cbee2ed
AhmadJamal01 <65499354+AhmadJamal01@users.noreply.github.com>
2021-01-08 14:20:46 +0200

    working on Homs.js to display today's donations

--- routes/Home.js
+++ routes/Home.js
@@ -1,8 +1,21 @@
 var express = require('express');
+const { rows } = require('mssql');
 var router = express.Router();
+var db = require('../DB/DatabaseConfig');
+
+//code to get today's date
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
+today = yyyy + '-' + mm + '-' + dd;
+document.write(today);
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  let query=`SELECT D.Blood_Type,COUNT * FROM DON_RECORD AS DR INNER JOIN DONOR AS D WHERE DR.SSN=D.SSN GROUP BY Blood_Type`;
+  db.all(qurey,)
   res.render('pages/Home',{title:"Blood Bank",css1:"home",css2:"Preq",css3:"animate",scrp:"home"})
 });
 
@@ -15,3 +28,5 @@ router.get('/ChangeAppointment', function(req, res, next) {
 });
 
 module.exports = router;
+
+
